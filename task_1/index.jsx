108850f61ebb11d0f23adb040a980e4b62664a92
Владислав Цой <tsoy.1996.vlad@gmail.com>
2022-05-18 16:42:35 +0600

    Устанил лишний рендер компонентов в task_1

--- task_1/index.jsx
+++ task_1/index.jsx
@@ -1,12 +1,16 @@
-import { Component } from 'react';
+import React, { Component, memo } from 'react';
 
 // functional component
-const FirstComponent = ({ name }) => (
-    <div>my name is {name}</div>
-);
+const FirstComponent = memo(({ name }) => (
+  <div>my name is {name}</div>
+));
 
 // class component
 class SecondComponent extends Component {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.name === this.props.name;
+    }
+
     render() {
         return (
             <div>my name is {this.props.name}</div>
