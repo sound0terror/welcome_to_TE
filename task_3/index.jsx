e78dedc144b78ce9646736b0533e54a7eb36ff24
Владислав Цой <tsoy.1996.vlad@gmail.com>
2022-05-18 17:05:33 +0600

    task_3: Обернул дочерний компонент в useMemo для избежания рендера.

--- task_3/index.jsx
+++ task_3/index.jsx
@@ -1,14 +1,15 @@
-import { useState, Fragment, memo } from 'react';
+import React, { useState, Fragment, memo, useMemo } from 'react';
 
 const MainComponent = ({
     user = { name: 'unknown', age: null } // default value for `props.user`
 }) => {
     const [ _, updateComponent ] = useState(); // change state for force component updating
+    const child = useMemo(() => <ChildComponent user={user}/>, [user.name, user.age]);
 
     return (
         <Fragment>
             <button onClick={updateComponent}>update MainComponent</button>
-            <ChildComponent user={user} />
+            {child}
         </Fragment>
     );
 };
